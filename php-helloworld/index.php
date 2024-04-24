a812ec1ec0adc8abc7fb0d3b7240070a635f623d
shhari <shiv.hari@live.com>
2024-04-24 11:39:45 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Heiiiiiiiiiiiiiiiiiiiiiiiillo, World! php version is " . PHP_VERSION . "\n";
+print "HeiiiiiiiiiiiiiiiiiiSUNLIFEADDDDDDDiiiiiillo, World! php version is " . PHP_VERSION . "\n";
 ?>
